96d8ded964f80de8d12e970afae5d5650ceb9a8f
junglesub <junglesubmarine@gmail.com>
2024-04-01 16:33:28 +0000

    remove base name

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Admin from "./Admin";
 import MainPage from "./MainPage";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <MainPage />,
-    },
-    {
-      path: "/admin",
-      element: <Admin />,
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: "/StroopTestWeb",
-  }
-);
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
